f94e50a26c59f82c8a32e8663fc9792861c44ba5
Shaun Wilkinson <shaun.wilkinson@sitekit.net>
2018-05-01 16:16:27 +0100

    Initial Commit

---
+++ GetGUID/GetGUID.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using System.Activities;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace SPandCRM
+{
+    public class GetGUID : CodeActivity
+    {
+        [Input("Record Dynamic Url")]
+        [RequiredArgument]
+        public InArgument<string> RecordUrl { get; set; }
+
+        [Output("Record Guid")]
+        public OutArgument<string> RecordGuid { get; set; }
+        
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            // Instantiate the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            if (tracingService == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve tracing service.");
+            }
+
+            tracingService.Trace("Entered GetRecordGuid.Execute(), Activity Instance Id: {0}, Workflow Instance Id: {1}",
+                executionContext.ActivityInstanceId,
+                executionContext.WorkflowInstanceId);
+
+            // Create the context 
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+
+            if (context == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve workflow context.");
+            }
+
+            tracingService.Trace("GetRecordGuid.Execute(), Correlation Id: {0}, Initiating User: {1}",
+                context.CorrelationId,
+                context.InitiatingUserId);
+
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                var entityReference = new DynamicUrlParser(RecordUrl.Get<string>(executionContext));
+
+                RecordGuid.Set(executionContext, entityReference.Id.ToString());
+            }
+            catch (FaultException<OrganizationServiceFault> e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+
+                // Handle the exception. 
+                throw;
+            }
+
+            tracingService.Trace("Exiting GetRecordGuid.Execute(), Correlation Id: {0}", context.CorrelationId);
+        }
+        
+    }
+}
--- GetGUID/Properties/AssemblyInfo.cs
+++ GetGUID/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GetGUID")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © SPandCRM.com 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
